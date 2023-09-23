a649ae4974224f81e8f40ffe4d808ad7443e2154
Mohamed Emara <medoemara104@gmail.com>
2023-09-23 20:01:16 +0300

    Solve the trianglar shape using .then() .catch() promises

--- Practice/promises/index.js
+++ Practice/promises/index.js
@@ -29,10 +29,120 @@ fs.readFile('./dog.txt', (err, data) => {
         .then(res => {  
             console.log(res.body.message);
 
-            
+
         })
         .catch(err =>  {
             console.log('Error XXXXXX');
         })
     
 })
+
+
+// -----------------------------------------------------------------------
+// Instead of this way, we will make another way using promises in reading files 
+
+// Let's create a function that returns a promise.
+// and use it in reading files..
+const readFilePro = (file) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, (err, data) => {
+            resolve(data);      // Whatever value we pass into the resolve function, 
+                                // is what will be later available as the argument in "then" method. 
+            // And this data here will be the value that this promsie returns to us.  
+            
+            // reject function is the function that's called when there is an error in the promise.
+            // which will be available later in "catch" method..
+            if(err) {
+                reject('I could not find that file 😔');
+            }
+        });
+
+
+    });
+};
+
+// The above function is just to know how promises work internally, but there is a built-in 
+// function that automatically promisify functions for us.
+
+
+// The write function needs another paramter which is the data to write in that file.
+const writeFilePro = (file, data) => {
+    return new Promise((resolve, reject) => {
+        // fs.writefile takes the two paramters (file, data) and one more parameter which is the callback function.
+        fs.readFile(file, data, (err) => {
+            if(err) reject('Could not write file 😔😔');
+            resolve('success ✅');
+        })
+    })
+}
+
+
+
+
+
+
+
+// Let's use readFilePro
+readFilePro(`${__dirname}/dog.txt`)
+    .then(data => {
+        superagent
+        .get(`https://dog.ceo/api/breed/${data}/images/random`)
+        .then(res => {  
+            console.log(res.body.message);
+
+
+        })
+        .catch(err =>  {
+            console.log('Error XXXXXX');
+        })
+    
+    })
+    .catch(err => {
+        console.log()
+    })
+
+
+
+// =====================================================================================
+
+// The problem with the previous function is that it still uses a callback inside a callback
+// To make it look better, we'll use chaining...
+// By making every function return a promise so that we can use .then afterwards..
+
+// The final version applies what we've just talked about.....
+
+
+readFilePro(`${__dirname}/dog.txt`) // we can use the first .then() as the readfilePro returns a promise.
+    .then(data => {
+        console.log(`Breed: ${data}`);
+        return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);   
+        // it returns a promose, so we can chain the next then()...
+    })
+    .then(res => {
+        console.log(res.body.message);
+        return appendFilePro('dog-file.txt', res.body.message);
+        // it also returns a promise, so we can chain the next .then()
+    })
+    .then(() => {
+        console.log('Random dog image saved to file!');
+    })
+    .catch(err => {
+        console.log('I cannot read the file 😔😔😔');
+    });
+    // Just one error handler (catch) to handle an error coming from each of the three promises..
+
+
+
+// Now let's create a same function but appends a new line with the image URL.. not just writing.
+
+
+
+
+const appendFilePro = (file, data) => {
+    return new Promise((resolve, reject) => {
+        fs.appendFile(file, data, err => {
+            if(err) reject('Cannot Write to the file 😔');
+            resolve('Write Successfully ✅');
+        });
+    })
+}
