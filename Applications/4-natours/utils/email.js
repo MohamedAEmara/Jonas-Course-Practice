41f0115c1b393ac84a05d4e508a358da2bdc8c9f
Mohamed Emara <medoemara104@gmail.com>
2024-01-13 10:33:18 +0200

    Refactor email util to send email witth Email class

--- Applications/4-natours/utils/email.js
+++ Applications/4-natours/utils/email.js
@@ -1,22 +1,76 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
+
+module.exports = class Email {
+    constructor(user, url) {
+        this.to = user.email;
+        this.firstName = user.name.split(' ')[0];
+        this.url = url;
+        this.from = process.env.EMAIL_FROM;
+    }
+
+    newTransport() {
+        if(process.env.NODE_ENV === 'production') {
+            // NOT IMPLEMENTED YET...
+            return 1; 
+        }
+
+        return nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            auth: {
+                user: process.env.EMAIL_USERNAME,
+                pass: process.env.EMAIL_PASSWORD
+            }
+        })
+    }
+
+    // Send the actual email:
+    async send(temlate, subject) {
+        // 1- Render HTML based on pug template 
+        const html = pug.renderFile(`${__dirname}/../views/emails/${template}.pug`, {
+            firstName: this.firstName,
+            url: this.url,
+            subject 
+        });
+
+        // 2- Define email options
+        const mailOptions = {
+            from: this.from,
+            to: this.to,               // it's passed to this funciton in Options object.
+            subject,               
+            html,
+            text: htmlToText.htmlToText(html)
+        }
+
+
+        // 3- Create a transport & send emails.
+        await this.newTransport().sendEmail(mailOptions)
+    }
+
+    async sendWelcom() {
+        await this.send('Welcome', 'Welcome to our App :)');
+    }
+} 
 
 const sendEmail = async options => {
     // There are 3 steps to send mails uing "nodemailer"..
      
-    // 1- Create a transporter
+    // // 1- Create a transporter
 
-    // FOR GMAIL....
+    // // FOR GMAIL....
     
-    const transporter = nodemailer.createTransport({
-        // here are the options..
-        host: 'smtp.gmail.com',
-        port: 465, 
-        secure: true,
-        auth: {
-            user: process.env.GMAIL_USERNAME,
-            pass: process.env.GMAIL_PASSWORD
-        }
-    });
+    // const transporter = nodemailer.createTransport({
+    //     // here are the options..
+    //     host: 'smtp.gmail.com',
+    //     port: 465, 
+    //     secure: true,
+    //     auth: {
+    //         user: process.env.GMAIL_USERNAME,
+    //         pass: process.env.GMAIL_PASSWORD
+    //     }
+    // });
     
 
 
