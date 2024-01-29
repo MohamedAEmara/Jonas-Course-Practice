efe7bc8efc65cccafb6c68c79b9d5f7a4218d221
Mohamed Emara <medoemara104@gmail.com>
2024-01-29 21:35:18 +0200

    Add create booking checkout controller

--- Applications/4-natours/controllers/bookingController.js
+++ Applications/4-natours/controllers/bookingController.js
@@ -1,5 +1,6 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('./../models/tourModel');
+const Booking = require('./../models/bookingModel');
 const catchAsync = require('./../utils/catchAsync');
 const factory = require('./handlerFactory');
 const AppError = require('./../utils/appError');
@@ -13,7 +14,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     const session = await stripe.checkout.sessions.create({
         // There are 3 options that are required here..
         payment_method_types: ['card'],
-        success_url: `${req.protocol}://${req.get('host')}/`,
+        // It's not safe to expose (success URL) to users, as some user may book a tour without purchasing...
+        // But it's a tmp solution so far. 
+        success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
         cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
         customer_email: req.user.email,
         client_reference_id: req.params.tourId,
@@ -34,4 +37,21 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
         status: 'success',
         session
     })
+});
+
+
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+    const { tour, user, price } = req.query;
+    // We now need to create a booking if all of these are specified...
+    if(!tour || !user || !price)
+        return next();
+
+    await Booking.create({
+        tour,
+        user,
+        price
+    });
+
+    res.redirect(req.originalUrl.split('?')[0]);        // To get the root (home page)
 });
